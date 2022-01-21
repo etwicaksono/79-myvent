70805822b0571b9fabeae38f2c14add1faca62ba
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-21 14:05:42 +0700

    done page add events

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,60 +1,92 @@
-@extends('layout.app',["title"=>"Home"])
+@extends('layout.app-no-container',["title"=>"Home"])
 
 @section('content')
-<p class="h1 my-2">List Pokemon</p>
+<div class="container px-5 d-flex vh-100 justify-content-center mt-lg-5">
+    <div class="row mt-lg-5 pt-lg-5">
+        <div class="col-lg-6 col-sm-12 col-md-12 p-0" data-aos="fade-left">
+            <div class="card">
+                <div class="card-body bg-main">
+                    <form>
+                        <div class="row">
+                            <div class="col-lg-6 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <label for="title" class="text-white">Title</label>
+                                    <input type="text" class="form-control" id="title"
+                                        placeholder="Enter event's title">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <label for="location" class="text-white">Location</label>
+                                    <input type="text" class="form-control" id="location"
+                                        placeholder="Enter event's location">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <label for="participants" class="text-white">Participants</label>
+                                    <input type="text" class="form-control" id="participants"
+                                        placeholder="Enter event's participants">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <label for="date" class="text-white">date</label>
+                                    <input type="text" class="form-control" id="date" placeholder="Enter event's date">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <label for="note" class="text-white">Note</label>
+                                    <textarea type="text" class="form-control" id="note"
+                                        placeholder="Enter event's note"></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <label for="picture" class="text-white">picture</label>
+                                    <input type="file" class="form-control" id="date" placeholder="Enter event's date">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-12 col-md-12" id="img-wrapper">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <button class="btn btn-info float-right">Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-6 col-sm-12 col-md-12 p-0" data-aos="fade-right">
+            <img src="{{ asset('img/discussion.png') }}" alt="Illustration" class="img-fluid">
 
-<div class="d-flex flex-row justify-content-center flex-wrap" id="pokemon-wrapper">
-
-</div>
-
-
-<div class="row justify-content-center">
-    <button class="btn btn-success my-5 align-self-center" id="btn-reload"><span class="fas fa-redo-alt"></span> Load
-        More</button>
+        </div>
+    </div>
 </div>
 @endsection
 
 @push('js')
+<script src="https://cdn.rawgit.com/michalsnik/aos/2.0.1/dist/aos.js"></script>
 <script>
     $(function(){
         let baseurl = "{{ url('') }}/"
-        let next = ""
 
-        $("#btn-reload").on("click",function(){
-            $.ajax({
-                url:next,
-                method:"get",
-                dataType:"json",
-                error:function(err){
-                    console.log(err);
-                },success:function(res){
-                    console.log(res);
-                    next = res.next
-
-                    let output=""
-                    $.each(res.results,function(key, value){
-                    let temp = value.url.slice(0,-1).split("/")
-                    let id = temp[temp.length-1];
-                    
-
-                    output+=`
-                    <a href="` + baseurl + `detail/`+ id +`">
-                        <div class="card m-2" style="width: 18rem;">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/` + id + `.png"
-                                class="card-img-top" alt="` + value.name +`">
-                            <div class="card-body">
-                                <p class="card-text text-center h3">` + value.name +`</p>
-                            </div>
-                        </div>
-                    </a>
-                    `
-
-                    })
-                    $("#pokemon-wrapper").append(output)
-
-                }
+        AOS.init({
+            duration: 1200,
             })
-        })
     })
 </script>
+@endpush
+
+@push('css')
+<link rel="stylesheet" href="https://cdn.rawgit.com/michalsnik/aos/2.0.1/dist/aos.css" />
 @endpush
--- resources/views/layout/app-no-container.blade.php
+++ resources/views/layout/app-no-container.blade.php
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>
         @if(!isset($title))
-        Pokepoke
+        Myvent
         @else
-        {{ $title }} | Pokepoke
+        {{ $title }} | Myvent
         @endif
     </title>
 
@@ -23,53 +23,61 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
+    <style>
+        .bg-main {
+            background-color: #baaeae;
+        }
+    </style>
+
+    @stack('css')
+
 </head>
 
 <body>
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="{{ url("") }}">Pokepoke</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <nav class="navbar  sticky-top navbar-expand-lg navbar-light bg-main mb-3">
+        <a class="navbar-brand font-weight-bolder text-white" href="{{ url("") }}">Myvent</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{ url('my-pokemon') }}">My Pokemon</a>
-                    </li>
-                    {{-- <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li> --}}
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
-                        id="search-input">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="btn-search">Search</button>
-                </form>
-            </div>
-        </nav>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                {{-- <li class="nav-item active">
+                    <a class="nav-link" href="{{ url('my-pokemon') }}">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li> --}}
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link font-weight-bolder" href="{{ url('my-pokemon') }}"><i class="fa fa-plus"></i>
+                        Add Event</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link text-white font-weight-bolder" href="{{ url('my-pokemon') }}">Dashboard</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
-        @yield('content')
-
-
-    </div>
+    @yield('content')
 
     {{-- <script src="{{ url("../vendor/components/jquery/jquery.min.js") }}"></script>
     <script src="{{ url("../vendor/twbs/bootstrap/dist/js/bootstrap.min.js") }}"></script> --}}
