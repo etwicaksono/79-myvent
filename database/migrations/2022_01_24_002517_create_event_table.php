c46aa76e21f3fa4cbccb5d1c3311c8dc742df742
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-24 07:28:14 +0700

    add event migration

--- database/migrations/2022_01_24_002517_create_event_table.php
+++ database/migrations/2022_01_24_002517_create_event_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateEventTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('event', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string("foto", 64);
-            $table->rememberToken();
+            $table->string("title", 128);
+            $table->string("location", 128);
+            $table->json("participant");
+            $table->date("date");
+            $table->text("note");
+            $table->string("picture", 64);
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('event');
     }
 }
