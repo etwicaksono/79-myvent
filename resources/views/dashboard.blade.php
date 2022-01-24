64cc99c77dc0c2359cf68ca34fdc9ee3f835615f
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-24 07:18:37 +0700

    setup dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,81 +1,36 @@
 @extends('layout.app-no-container',["title"=>"Dashboard"])
 
 @section('content')
-<div class="container px-5 d-flex vh-100 justify-content-center mt-lg-5">
-    <div class="row mt-lg-5 pt-lg-5">
-        <div class="col-lg-6 col-sm-12 col-md-12 p-0" data-aos="fade-left">
-            <div class="card">
-                <div class="card-body bg-main">
-                    <form>
-                        <div class="row">
-                            <div class="col-lg-6 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <label for="title" class="text-white">Title</label>
-                                    <input type="text" class="form-control" id="title"
-                                        placeholder="Enter event's title">
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <label for="location" class="text-white">Location</label>
-                                    <input type="text" class="form-control" id="location"
-                                        placeholder="Enter event's location">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <label for="participants" class="text-white">Participants</label>
-                                    <input type="text" class="form-control" id="participants"
-                                        placeholder="Enter event's participants">
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <label for="date" class="text-white">date</label>
-                                    <input type="text" class="form-control" id="date" placeholder="Enter event's date">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label for="note" class="text-white">Note</label>
-                                    <textarea type="text" class="form-control" id="note"
-                                        placeholder="Enter event's note"></textarea>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6 col-sm-12 col-md-12">
-                                <div class="form-group">
-                                    <label for="picture" class="text-white">picture</label>
-                                    <input type="file" class="form-control" id="date" placeholder="Enter event's date">
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-sm-12 col-md-12" id="img-wrapper">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <button class="btn btn-info float-right">Submit</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-6 col-sm-12 col-md-12 p-0" data-aos="fade-right">
-            <img src="{{ asset('img/discussion.png') }}" alt="Illustration" class="img-fluid">
-
-        </div>
-    </div>
+<div class="container px-5 mt-lg-5">
+    <table id="table_id" class="display table">
+        <thead class="bg-main">
+            <tr>
+                <th style="width: 0%">No</th>
+                <th style="width: 20%">Title</th>
+                <th style="width: 20%">Location</th>
+                <th style="width: 20%">Date</th>
+                <th style="width: 20%">Paticipant</th>
+                <th style="width: 20%">Note</th>
+            </tr>
+        </thead>
+        <tbody>
+            @for ($i = 0; $i < 100; $i++) <tr>
+                <td>{{ $i + 1 }}</td>
+                <td>Row {{ $i }} Data 2</td>
+                <td>Row {{ $i }} Data 3</td>
+                <td>Row {{ $i }} Data 4</td>
+                <td>Row {{ $i }} Data 5</td>
+                <td>Row {{ $i }} Data 6</td>
+                </tr>
+                @endfor
+        </tbody>
+    </table>
 </div>
 @endsection
 
 @push('js')
 <script src="https://cdn.rawgit.com/michalsnik/aos/2.0.1/dist/aos.js"></script>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
 <script>
     $(function(){
         let baseurl = "{{ url('') }}/"
@@ -83,10 +38,23 @@
         AOS.init({
             duration: 1200,
             })
+
+        $('#table_id').DataTable({
+            dom:`<"row"<"col"<"float-left mb-3" f>>>
+                    rt
+                    <"row"<"col"<"d-flex justify-content-center mt-3" p>>>`,
+            language: {
+                'paginate': {
+                'previous': '<span class="fas fa-caret-left fa-lg"></span>',
+                'next': '<span class="fas fa-caret-right fa-lg"></span>'
+                }
+            }
+        });
     })
 </script>
 @endpush
 
 @push('css')
-<link rel="stylesheet" href="https://cdn.rawgit.com/michalsnik/aos/2.0.1/dist/aos.css" />
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
+
 @endpush
